67ceb836246434cce761d96cd586c832359c72c3
Christian Nolte <hello@noltech.net>
2016-10-07 22:06:56 +0200

    Check if wp-config.php not exists.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-if ! [ -e /var/www/html/installer.php ]; then
-  echo >&2 "--------------------------------------------------"
+if [[ ! -e /var/www/html/installer.php && ! -e /var/www/html/wp-config.php ]]; then
+  echo >&2 "---------------------------------------------------------"
   echo >&2 ""
-  echo >&2 "No installer.php found."
+  echo >&2 "No wordpress installation or installer.php found."
   echo >&2 ""
-  echo >&2 "--------------------------------------------------"
+  echo >&2 "---------------------------------------------------------"
   exit 1
 fi
 
