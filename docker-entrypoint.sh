fbae34d9179843fc18d06aa5b4092332c6cb5428
Christian Nolte <hello@noltech.net>
2016-10-15 10:38:37 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,6 @@ if [[ ! -e /var/www/html/installer.php && ! -e /var/www/html/wp-config.php ]]; t
   echo >&2 "No wordpress installation or installer.php found."
   echo >&2 ""
   echo >&2 "---------------------------------------------------------"
-  exit 1
 fi
 
 exec "$@"
