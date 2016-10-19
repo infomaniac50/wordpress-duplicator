b2381d68cdf426b8465b2c0c0d60b4b36b6bcb7f
Christian Nolte <hello@noltech.net>
2016-10-19 10:27:55 +0200

    Fix typo.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [[ ! -e /var/www/html/wp-config.php && ! -e /var/www/html/installer.php && ]]
+if [[ ! -e /var/www/html/wp-config.php && ! -e /var/www/html/installer.php ]]
 then
   cp /installer.php /var/www/html/installer.php
 fi
